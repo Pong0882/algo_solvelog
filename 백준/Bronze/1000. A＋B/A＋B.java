1ecbd8e7960e19a0b3bae4111040c3ed62c99df5
HelloPong <77256939+Pong0882@users.noreply.github.com>
2026-03-21 18:20:32 +0900

    [Bronze V] Title: A+B, Time: 68 ms, Memory: 11544 KB -BaekjoonHub

--- 백준/Bronze/1000. A＋B/A＋B.java
+++ 백준/Bronze/1000. A＋B/A＋B.java
@@ -3,6 +3,7 @@ import java.io.IOException;
 import java.io.InputStreamReader;
 import java.util.StringTokenizer;
 
+//github action test
 public class Main {
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
